3b10235d4cc9bd365b226fd13721eb8e3c461f9b
RomanVas30 <37067022+RomanVas30@users.noreply.github.com>
2018-03-30 03:46:57 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -1,1 +1,21 @@
+enum col { BLUE, GREEN, WHITE, RED, CYAN };
+namespace BSTree {
+struct Node {
+  int data;
+  Node* left;
+  Node* right;
+};
+
+class Tree {
+  Node* root;
+
+ public:
+  Tree();
+  auto insert(int value) -> bool;
+  auto add(Node*&, int) -> void;
+  auto print_(Node*, int) -> void;
+  auto print() -> void;
+  ~Tree();
+};
+}
 
