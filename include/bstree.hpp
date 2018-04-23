4e4426169297f68e23b6eee9e179ff5f78282a98
RomanVas30 <37067022+RomanVas30@users.noreply.github.com>
2018-04-23 23:34:26 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -1,23 +1,26 @@
 enum col { BLUE, GREEN, WHITE, RED, CYAN };
 namespace BSTree {
-struct Node {
-  int data;
-  Node* left;
-  Node* right;
-};
+    enum class traversal_order {pre, in, post};
+    struct Node {
+        int data;
+        Node* left;
+        Node* right;
+    };
 
-class Tree {
-  Node* root;
+    class Tree {
+        Node* root;
 
- public:
-  Tree();
-  auto insert(int value) -> bool;
-  auto add(Node*&, int) -> void;
-  auto print_(Node*, int) -> void;
-  auto print() -> void;
-  auto empty() -> bool;
-  auto delete_Tree(Node* curr) -> void;
-  ~Tree();
-};
+        public:
+        Tree();
+        auto insert(int value) -> void;
+        auto add(Node*&, int) -> void;
+        auto print_(Node*, int) -> void;
+        auto print() -> void;
+        auto empty() -> bool;
+        auto add_element(int value) -> bool;
+        auto remove_element(int value) -> bool;
+        auto delete_Tree(Node* curr) -> void;
+        auto list (traversal_order order) -> void;
+        ~Tree();
+    };
 }
-
