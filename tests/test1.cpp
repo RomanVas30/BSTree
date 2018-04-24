ee61425d1098ea6759d5490a639f28bbd97bdc26
RomanVas30 <37067022+RomanVas30@users.noreply.github.com>
2018-04-24 20:55:56 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,5 +1,7 @@
 #include <catch.hpp>
 #include <sstream>
+#include <string>
+#include <fstream>
 
 #include "bstree.hpp"
 using namespace BSTree;
@@ -7,15 +9,79 @@ using namespace BSTree;
 TEST_CASE("empty tree")
 {
 	Tree tree;
-	REQUIRE( tree.empty() == true );
+	REQUIRE( tree.exists() == true );
 }
 
 TEST_CASE("initializing tree nodes")
 {
-	Tree tree;
+  Tree tree;
   for (unsigned int i = 1; i < 5; ++i)
   tree.insert(i);
   
-  REQUIRE( tree.empty() == false );
+  REQUIRE( tree.exists() == false );
 
 }
+
+TEST_CASE("detour_pre")
+{
+  Tree tree;
+  tree.insert(3);
+  tree.insert(2);
+  tree.insert(1);
+  tree.insert(4);
+  tree.insert(5);
+	
+  std::ofstream out("file.txt");
+  out >> tree.print(traversal_order::pre);
+  out.close();
+	
+  std::string result;
+  std::string text = "3 2 1 4 5 ";
+  std::ifstream in("file.txt");
+  getline(in, result);
+  
+  REQUIRE(result == text);
+}
+
+TEST_CASE("detour_in")
+{
+  Tree tree;
+  tree.insert(3);
+  tree.insert(2);
+  tree.insert(1);
+  tree.insert(4);
+  tree.insert(5);
+	
+  std::ofstream out("file.txt");
+  out >> tree.print(traversal_order::in);
+  out.close();
+	
+  std::string result;
+  std::string text = "1 2 3 4 5 ";
+  std::ifstream in("file.txt");
+  getline(in, result);
+  
+  REQUIRE(result == text);
+}
+
+TEST_CASE("detour_post")
+{
+  Tree tree;
+  tree.insert(3);
+  tree.insert(2);
+  tree.insert(1);
+  tree.insert(4);
+  tree.insert(5);
+	
+  std::ofstream out("file.txt");
+  out >> tree.print(traversal_order::post);
+  out.close();
+	
+  std::string result;
+  std::string text = "1 2 5 4 3 ";
+  std::ifstream in("file.txt");
+  getline(in, result);
+  
+  REQUIRE(result == text);
+}
+    
