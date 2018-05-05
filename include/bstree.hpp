3581101c825da1efe8d6ebd266c3aab8649685f2
RomanVas30 <37067022+RomanVas30@users.noreply.github.com>
2018-05-06 00:15:05 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -1,3 +1,7 @@
+#include <fstream>
+#include <initializer_list>
+#include <cstddef>
+
 enum col { BLUE, GREEN, WHITE, RED, CYAN };
 namespace BSTree {
     enum class traversal_order {pre, in, post};
@@ -9,15 +13,21 @@ namespace BSTree {
 
     class Tree {
         Node* root;
-
         public:
         Tree();
+        Tree(std::initializer_list<int> list);
+        Tree(Tree& tree);
+        auto empty() -> bool;
         auto insert(int value) -> void;
-        auto exists() -> bool;
         auto print_tree() -> void;
+        auto exists(int value) -> bool;
         auto remove(int value) -> bool;
         auto add_element(int value) -> bool;
         auto print(traversal_order order) -> void;
+        auto save(const std::string& path) -> bool;
+        auto load(const std::string& path) -> bool;
+        auto friend operator<<(std::ostream&,const Tree&) -> std::ostream&;
+        auto operator=(Tree&) -> Tree&;
         ~Tree();
     };
 }
